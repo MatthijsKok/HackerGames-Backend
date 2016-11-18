10826883b9fa04154069a7522f286a07828ddeac
BernardBot <bernardo.reinier@gmail.com>
2016-11-18 20:30:38 +0000

    Internet class implemented

--- src/hackergames/api/ApiInterface.java
+++ src/hackergames/api/ApiInterface.java
@@ -1,12 +1,17 @@
 package hackergames.api;
 
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.List;
 
 
 public class ApiInterface
 {
-    static Internet getInternet(String url) {
-        //
+    static Internet getInternet(String urlExtra, JSONObject parameters) throws MalformedURLException {
+        URL url = new URL("https://hackathon.dominos.cloud" + urlExtra);
+
     }
 
     List<Pizza> getAll()
