58377ac3ce4afd017bbf38cff8c0cfda722bac19
Jeroen <jeroenesseveld@gmail.com>
2016-11-18 22:00:20 +0100

    added resources

--- src/main/java/com/hackergames/BackendServerApplication.java
+++ src/main/java/com/hackergames/BackendServerApplication.java
@@ -2,11 +2,24 @@ package com.hackergames;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class BackendServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendServerApplication.class, args);
+	}
+
+	@RequestMapping(value = "/pizza")
+	public String available() {
+		return "pizza route";
+	}
+
+	@RequestMapping(value = "/room")
+	public String checkedOut() {
+		return "room route!";
 	}
 }
