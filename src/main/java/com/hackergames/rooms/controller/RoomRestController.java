4983e8f1913c537e1e2a7dc3145bdcb308c287a5
Felix Dekker <felix@fwdekker.com>
2016-11-19 11:31:18 +0100

    Complete response

--- src/main/java/com/hackergames/rooms/controller/RoomRestController.java
+++ src/main/java/com/hackergames/rooms/controller/RoomRestController.java
@@ -3,6 +3,8 @@ package com.hackergames.rooms.controller;
 import com.hackergames.pizzas.model.Pizza;
 import com.hackergames.rooms.model.Room;
 import com.hackergames.rooms.service.RoomServiceImpl;
+import com.hackergames.util.ResponseEntityBuilder;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -44,15 +46,15 @@ public class RoomRestController {
     }
 
     @PostMapping("/room/{roomID}/pizza")
-    public ResponseEntity<String> addPizza(@PathVariable Long roomID, @RequestParam String name,
-                                           @RequestParam String size, @RequestParam(required = false, defaultValue = "") ArrayList<String> additions) {
+    public ResponseEntity<?> addPizza(@PathVariable Long roomID, @RequestParam String name,
+                                      @RequestParam String size, @RequestParam(required = false, defaultValue = "") ArrayList<String> additions) {
         Pizza pizza = Pizza.fromName(name, size, additions);
         if (pizza == null) {
-            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+            return ResponseEntityBuilder.createResponseEntity(HttpStatus.BAD_REQUEST, "Invalid pizza name");
         }
 
         roomService.addPizza(roomID, pizza);
-        return new ResponseEntity<>(pizza.toString(), HttpStatus.OK);
+        return ResponseEntityBuilder.createResponseEntity(HttpStatus.OK, JSONObject.wrap(pizza).toString());
     }
 
     @DeleteMapping("/room/{roomID}/pizza")
