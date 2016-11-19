cae7137c8586c9c32362f102f7e5428ae2de2dd4
Matthijs Kok <matthijs.kok@me.com>
2016-11-19 09:14:19 +0100

    Add Pizza object

--- src/main/java/com/hackergames/pizzas/model/Pizza.java
+++ src/main/java/com/hackergames/pizzas/model/Pizza.java
@@ -1,75 +1,15 @@
 package com.hackergames.pizzas.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.json.JSONObject;
+import lombok.Setter;
 
-
+@AllArgsConstructor
 public class Pizza
 {
-    @Getter
-    private String name;
-    @Getter
-    private String description;
-    @Getter
-    private String image;
-    @Getter
-    private String pickupPrice;
-    @Getter
-    private String deliveryPrice;
-    @Getter
-    private String status;
-    @Getter
-    private String componentStatus;
-    @Getter
-    private String itemType;
-    @Getter
-    private String itemCode;
-    @Getter
-    private String subMenu;
-    @Getter
-    boolean hnhAble;
 
+    @Getter
+    @Setter
+    public String name;
 
-    /**
-     * Creates a new {@code Pizza} from a JSON string.
-     *
-     * @param jso     the JSON string
-     * @param subMenu the type of pizza
-     * @return a new {@code Pizza}
-     */
-    public static Pizza fromJson(final JSONObject jso, final String subMenu)
-    {
-        JSONObject price = jso.getJSONObject("Price");
-        JSONObject linkedItem = jso.getJSONObject("LinkedItem");
-
-        return new Pizza(
-                jso.getString("Name"),
-                jso.getString("Description"),
-                jso.getString("Image"),
-                price.getString("Pickup"),
-                price.getString("Delivered"),
-                jso.getString("Status"),
-                jso.getString("ComponentStatus"),
-                linkedItem.getString("ItemType"),
-                linkedItem.getString("ItemCode"),
-                subMenu,
-                jso.getBoolean("HalfnHalfEnabled")
-        );
-    }
-
-    public Pizza(String name, String description, String image, String pickupPrice, String deliveryPrice, String status,
-                 String componentStatus, String itemType, String itemCode, String subMenu, boolean hnhAble)
-    {
-        this.name = name;
-        this.description = description;
-        this.image = image;
-        this.pickupPrice = pickupPrice;
-        this.deliveryPrice = deliveryPrice;
-        this.status = status;
-        this.componentStatus = componentStatus;
-        this.itemType = itemType;
-        this.itemCode = itemCode;
-        this.subMenu = subMenu;
-        this.hnhAble = hnhAble;
-    }
 }
