ed842633a2472d5586854e7d1243dfadc7e50750
Matthijs Kok <matthijs.kok@me.com>
2016-11-19 10:06:47 +0100

    make our API REST compliant....ish

--- src/main/java/com/hackergames/rooms/controller/RoomRestController.java
+++ src/main/java/com/hackergames/rooms/controller/RoomRestController.java
@@ -6,11 +6,8 @@ import com.hackergames.rooms.service.RoomServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
 @CrossOrigin
-@RequestMapping("/room")
 public class RoomRestController {
 
     private RoomServiceImpl roomService;
@@ -20,39 +17,36 @@ public class RoomRestController {
         this.roomService = roomService;
     }
 
-    @GetMapping("/new")
+    @PostMapping("/rooms")
     public Room createRoom() {
         return roomService.createNewRoom();
     }
 
-    @DeleteMapping("/all")
+    @DeleteMapping("/rooms")
     public void deleteAllRooms() {
         roomService.deleteAllRooms();
     }
 
-    @DeleteMapping("/{roomID}")
+
+    @DeleteMapping("/room/{roomID}")
     public void deleteRoom(@PathVariable Long roomID) {
         roomService.deleteRoom(roomID);
     }
 
-    @GetMapping("/{roomID}")
+    @GetMapping("/room/{roomID}")
     public Room getRoom(@PathVariable Long roomID) {
         return roomService.getRoom(roomID);
     }
 
-    @PutMapping("/{roomID}/{pizza}")
-    public Pizza addPizza(@PathVariable Long roomID, @PathVariable Pizza pizza) {
+
+    @PostMapping("/room/{roomID}/pizza")
+    public Pizza addPizza(@PathVariable Long roomID, @RequestBody Pizza pizza) {
         roomService.addPizza(roomID, pizza);
         return pizza;
     }
 
-    @DeleteMapping("/{roomID}/{pizza}")
-    public void deletePizza(@PathVariable Long roomID, @PathVariable Pizza pizza) {
+    @DeleteMapping("/room/{roomID}/pizza")
+    public void deletePizza(@PathVariable Long roomID, @RequestBody Pizza pizza) {
         roomService.deletePizza(roomID, pizza);
-    }
-
-    @GetMapping("/{roomID/all}")
-    public List<Pizza> getAllPizzas(@PathVariable Long roomID) {
-        return roomService.getAllPizzas(roomID);
     }
 }
